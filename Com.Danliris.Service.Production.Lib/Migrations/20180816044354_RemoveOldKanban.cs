92566be17bdedfe80e43c70c0da092f6ac4bbb79
leslie.aula <leslie.aula@moonlay.com>
2018-08-16 11:47:22 +0700

    Update Old Kanban

---
+++ Com.Danliris.Service.Production.Lib/Migrations/20180816044354_RemoveOldKanban.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Production.Lib.Migrations
+{
+    public partial class RemoveOldKanban : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Kanbans_Kanbans_OldKanbanId",
+                table: "Kanbans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Kanbans_OldKanbanId",
+                table: "Kanbans");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Kanbans_OldKanbanId",
+                table: "Kanbans",
+                column: "OldKanbanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Kanbans_Kanbans_OldKanbanId",
+                table: "Kanbans",
+                column: "OldKanbanId",
+                principalTable: "Kanbans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
