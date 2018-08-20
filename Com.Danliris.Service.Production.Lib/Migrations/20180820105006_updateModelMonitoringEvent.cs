3c40ebac094457794e6ae0038c73adfcb687a683
adityahenanda <henanda.aditya@gmail.com>
2018-08-20 17:58:05 +0700

    update validation and model monitoring event

---
+++ Com.Danliris.Service.Production.Lib/Migrations/20180820105006_updateModelMonitoringEvent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Production.Lib.Migrations
+{
+    public partial class updateModelMonitoringEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductionOrderDeliveryDate",
+                table: "MonitoringEvent",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductionOrderDeliveryDate",
+                table: "MonitoringEvent");
+        }
+    }
+}
