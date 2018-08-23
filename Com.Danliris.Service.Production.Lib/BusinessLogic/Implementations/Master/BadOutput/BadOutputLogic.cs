a2d48230b1573b50dd552581daffc33aa36d2915
adityahenanda <henanda.aditya@gmail.com>
2018-08-23 17:58:10 +0700

    add bad output facade, model , view model
    add bad output facade, model , view model

---
+++ Com.Danliris.Service.Production.Lib/BusinessLogic/Implementations/Master/BadOutput/BadOutputLogic.cs
@@ -0,0 +1,22 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.Models.Master.BadOutput;
+using Com.Danliris.Service.Production.Lib;
+using Com.Danliris.Service.Production.Lib.Services.IdentityService;
+using Com.Danliris.Service.Production.Lib.Utilities.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.BusinessLogic.Implementations.Master.BadOutput
+{
+    public class BadOutputLogic : BaseLogic<BadOutputModel>
+    {
+        private const string UserAgent = "production-service";
+        private BadOutputMachineLogic BadOutputMachineLogic;
+        public BadOutputLogic(BadOutputMachineLogic badOutputMachineLogic, IIdentityService identityService, ProductionDbContext dbContext) : base(identityService, dbContext)
+        {
+            this.BadOutputMachineLogic = badOutputMachineLogic;
+        }
+
+    }
+}
---
+++ Com.Danliris.Service.Production.Lib/Models/Daily_Operation/DailyOperationBadOutputReasonsModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.Models.Daily_Operation
+{
+    public class DailyOperationBadOutputReasonsModel
+    {
+    }
+}
