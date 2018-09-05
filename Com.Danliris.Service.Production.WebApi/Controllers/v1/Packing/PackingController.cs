c3e480d89c443709ad3c08c1829b7288fe88c135
JulioJLM <julio.lemena@moonlay.com>
2018-09-05 16:51:24 +0700

    Add pdf for packing

--- Com.Danliris.Service.Production.WebApi/Controllers/v1/Packing/PackingController.cs
+++ Com.Danliris.Service.Production.WebApi/Controllers/v1/Packing/PackingController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Com.Danliris.Service.Finishing.Printing.Lib.BusinessLogic.Interfaces.Packing;
 using Com.Danliris.Service.Finishing.Printing.Lib.Models.Packing;
+using Com.Danliris.Service.Finishing.Printing.Lib.PdfTemplates;
 using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.Packing;
 using Com.Danliris.Service.Production.Lib.Services.IdentityService;
 using Com.Danliris.Service.Production.Lib.Services.ValidateService;
@@ -23,6 +25,39 @@ namespace Com.Danliris.Service.Finishing.Printing.WebApi.Controllers.v1.Packing
     {
         public PackingController(IIdentityService identityService, IValidateService validateService, IPackingFacade facade, IMapper mapper) : base(identityService, validateService, facade, mapper, "1.0.0")
         {
+        }
+
+        [HttpGet("pdf/{Id}")]
+        public new async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            try
+            {
+                PackingModel model = await Facade.ReadByIdAsync(id);
+                if (model == null)
+                {
+                    Dictionary<string, object> Result =
+                        new ResultFormatter(ApiVersion, General.NOT_FOUND_STATUS_CODE, General.NOT_FOUND_MESSAGE)
+                        .Fail();
+                    return NotFound(Result);
+                }
+                else
+                {
+                    int timeoffsset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
+                    PackingPdfTemplate pdfTemplate = new PackingPdfTemplate(model, timeoffsset);
+                    MemoryStream stream = pdfTemplate.GeneratePdfTemplate();
+                    return new FileStreamResult(stream, "application/pdf")
+                    {
+                        FileDownloadName = string.Format("{0}.pdf", model.Code)
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
         }
     }
 }
