9ab3389575d234fe75e5c46c7b1a84386919777b
Ali Reza <ali.syariati@moonlay.com>
2018-09-25 20:42:13 +0700

    Add ReturToQC View Model

---
+++ Com.Danliris.Service.Production.Lib/ViewModels/ReturToQC/ReturToQCViewItemDetailModel.cs
@@ -0,0 +1,65 @@
+﻿using Com.Danliris.Service.Production.Lib.Utilities.BaseClass;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.ReturToQC
+{
+    public class ReturToQCItemDetailViewModel : BaseViewModel, IValidatableObject
+    {
+        public ReturToQCItemDetailViewModel()
+        {
+
+        }
+
+        public string ColorWay { get; set; }
+
+        public string DesignCode { get; set; }
+
+        public int DesignNumber { get; set; }
+
+        public double Length { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int ProductName { get; set; }
+
+        public int QuantityBefore { get; set; }
+
+        [StringLength(500)]
+        public string Remark { get; set; }
+
+        public int ReturQuantity { get; set; }
+
+        public int StorageId { get; set; }
+
+        public string UOM { get; set; }
+
+        public int UOMId { get; set; }
+
+        public double Weight
+        {
+            get { return Weight; }
+            set
+            {
+                if (Weight <= 0)
+                {
+                    Weight = 0;
+                }
+            }
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (QuantityBefore < ReturQuantity)
+                yield return new ValidationResult("ReturQuantity tidak boleh lebih dari stockQuantity", new List<string> { "ReturQuantity" });
+
+            if (string.IsNullOrWhiteSpace(Remark))
+                yield return new ValidationResult("Remark tidak boleh kosong", new List<string> { "Remark" });
+
+            if (Length <= 0)
+                yield return new ValidationResult("Length tidak boleh kosong", new List<string> { "Length" });
+
+
+        }
+    }
+}
---
+++ Com.Danliris.Service.Production.Lib/ViewModels/ReturToQC/ReturToQCViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.Integration.Master;
+using Com.Danliris.Service.Production.Lib.Utilities.BaseClass;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.ReturToQC
+{
+    public class ReturToQCViewModel : BaseViewModel, IValidatableObject
+    {
+        public ReturToQCViewModel()
+        {
+        }
+
+        public DateTimeOffset Date { get; set; }
+
+        public string DeliveryOrderNo { get; set; }
+
+        public string Destination { get; set; }
+
+        public string FinishedGoodCode { get; set; }
+
+        public bool IsVoid { get; set; }
+
+        public MaterialIntegrationViewModel Material { get; set; }
+
+        [StringLength(25)]
+        public string MaterialWidthFinish { get; set; }
+
+        [StringLength(500)]
+        public string Remark { get; set; }
+
+        [StringLength(25)]
+        public string ReturNo { get; set; }
+
+        public MaterialConstructionIntegrationViewModel MaterialConstruction { get; set; }
+
+        public ICollection<ReturToQCItemViewModel> Items { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Material == null)
+                yield return new ValidationResult("Material harus diisi", new List<string> { "Material" });
+            else
+            {
+                if (Material.Id == 0)
+                    yield return new ValidationResult("Material harus diisi", new List<string> { "Material" });
+            }
+
+            if (MaterialConstruction == null)
+                yield return new ValidationResult("Construction harus diisi", new List<string> { "MaterialConstruction" });
+            else
+            {
+                if (MaterialConstruction.Id == 0)
+                    yield return new ValidationResult("Construction harus diisi", new List<string> { "MaterialConstruction" });
+            }
+
+            if(string.IsNullOrWhiteSpace(Destination))
+                yield return new ValidationResult("Destination harus diisi", new List<string> { "Destination" });
+
+            if(Date == null || Date == DateTimeOffset.MinValue)
+                yield return new ValidationResult("Date harus diisi", new List<string> { "Date" });
+
+            if(string.IsNullOrWhiteSpace(MaterialWidthFinish))
+                yield return new ValidationResult("MaterialWidthFinish harus diisi", new List<string> { "MaterialWidthFinish" });
+        }
+    }
+}
