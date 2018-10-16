d2f17957a5c6225492b0bb3793207dd6069a52c8
leslie.aula <leslie.aula@moonlay.com>
2018-10-16 15:19:51 +0700

    Add Coverage

--- Com.Danliris.Service.Production.Test/Controllers/DailyOperationControllerTest.cs
+++ Com.Danliris.Service.Production.Test/Controllers/DailyOperationControllerTest.cs
@@ -20,27 +20,56 @@ namespace Com.Danliris.Service.Finishing.Printing.Test.Controllers
     public class DailyOperationControllerTest : BaseControllerTest<DailyOperationController, DailyOperationModel, DailyOperationViewModel, IDailyOperationFacade>
     {
 
-        //[Fact]
-        //public void GetReport_WithoutException_ReturnOK()
-        //{
-        //    var mockFacade = new Mock<IDailyOperationFacade>();
-        //    mockFacade.Setup(x => x.GetReport(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>()))
-        //        .Returns(new ReadResponse<DailyOperationViewModel>(new List<DailyOperationViewModel>(), 0, new Dictionary<string, string>(), new List<string>()));
-        //    var mockMapper = new Mock<IMapper>();
-        //    var mockIdentityService = new Mock<IIdentityService>();
-        //    var mockValidateService = new Mock<IValidateService>();
+        [Fact]
+        public void GetReport_WithoutException_ReturnOK()
+        {
+            var mockFacade = new Mock<IDailyOperationFacade>();
+            mockFacade.Setup(x => x.GetReport(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>()))
+                .Returns(new ReadResponse<DailyOperationViewModel>(new List<DailyOperationViewModel>(), 0, new Dictionary<string, string>(), new List<string>()));
 
-        //    DailyOperationController controller = new DailyOperationController(mockIdentityService.Object, mockValidateService.Object, mockFacade.Object, mockMapper.Object)
-        //    {
-        //        ControllerContext = new ControllerContext()
-        //        {
-        //            HttpContext = new DefaultHttpContext()
-        //        }
-        //    };
-        //    controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = "1";
+            var mockMapper = new Mock<IMapper>();
 
-        //    var response = controller.GetReport();
-        //    Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
-        //}
+            var mockIdentityService = new Mock<IIdentityService>();
+
+            var mockValidateService = new Mock<IValidateService>();
+
+            DailyOperationController controller = new DailyOperationController(mockIdentityService.Object, mockValidateService.Object, mockFacade.Object, mockMapper.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = $"{It.IsAny<int>()}";
+
+            var response = controller.GetReport(It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>());
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void GetReport_WithException_ReturnError()
+        {
+            var mockFacade = new Mock<IDailyOperationFacade>();
+            mockFacade.Setup(x => x.GetReport(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>()))
+                .Throws(new Exception());
+
+            var mockMapper = new Mock<IMapper>();
+
+            var mockIdentityService = new Mock<IIdentityService>();
+
+            var mockValidateService = new Mock<IValidateService>();
+
+            DailyOperationController controller = new DailyOperationController(mockIdentityService.Object, mockValidateService.Object, mockFacade.Object, mockMapper.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = $"{It.IsAny<int>()}";
+
+            var response = controller.GetReport(It.IsAny<DateTime?>(), It.IsAny<DateTime?>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>());
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
     }
 }
