8d7b591a71c8d0edf786c2fe6535e96c27b8763f
LeslieAula <LeslieAula@DESKTOP-FKFTBC0>
2019-05-10 15:06:40 +0700

    Add Index Item Migration

--- Com.Danliris.Service.Production.Lib/Migrations/20190429081800_AddIndexItemMigrationPurpose.Designer.cs
+++ Com.Danliris.Service.Production.Lib/Migrations/20190429081800_AddIndexItemMigrationPurpose.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Production.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Finishing.Printing.Lib.Migrations
 {
     [DbContext(typeof(ProductionDbContext))]
-    partial class ProductionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190429081800_AddIndexItemMigrationPurpose")]
+    partial class AddIndexItemMigrationPurpose
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -725,8 +727,6 @@ namespace Com.Danliris.Service.Finishing.Printing.Lib.Migrations
                         .HasMaxLength(500);
 
                     b.Property<int>("SelectedIndex");
-
-                    b.Property<int>("StepIndex");
 
                     b.HasKey("Id");
 
