2b45eda8dbcc94ae44833cb33fdf6b6fbbf9d01a
Leslie Aula <cristiano.ulo@gmail.com>
2019-10-30 22:50:48 +0700

    Add Daily Operation Kanban Step Index

---
+++ Com.Danliris.Service.Production.Lib/Migrations/20191030154917_AddDailyOperation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.Migrations
+{
+    public partial class AddDailyOperation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KanbanStepIndex",
+                table: "DailyOperation",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KanbanStepIndex",
+                table: "DailyOperation");
+        }
+    }
+}
