2dacfc8a9a977c715552e030e9eaebcd482919e4
Franciskus Napitupulu <8franciskusna70@gmail.com>
2020-04-08 20:09:36 +0700

    AddApiInShipmentDocumentForSalesReceipt3

---
+++ Com.Danliris.Service.Production.Test/Services/NewShipmentDocumentPackingReceiptItemProductServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.NewShipmentDocument;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.Services
+{
+    public class NewShipmentDocumentPackingReceiptItemProductServiceTest
+    {
+        [Fact]
+        public void Should_Success_Instanciate_NewShipmentDocumentPackingReceiptItemProduct_ViewModel()
+        {
+
+            var viewModel = new NewShipmentDocumentPackingReceiptItemProductViewModel()
+            {
+                ProductName = "ProductName",
+                Quantity = 1,
+                QuantityUOM = "QuantityUOM",
+                Total = 100,
+                UOMId = 1,
+                UOMUnit = "UOMUnit",
+            };
+
+            Assert.NotNull(viewModel.ProductName);
+            Assert.NotNull(viewModel.Quantity);
+            Assert.NotNull(viewModel.QuantityUOM);
+            Assert.NotNull(viewModel.Total);
+            Assert.NotNull(viewModel.UOMId);
+            Assert.NotNull(viewModel.UOMUnit);
+        }
+    }
+}
