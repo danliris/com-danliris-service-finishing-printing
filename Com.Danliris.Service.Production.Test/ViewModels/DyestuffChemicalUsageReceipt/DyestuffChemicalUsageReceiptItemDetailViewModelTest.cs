fbefe99ffd0cf8b672255e57239fbe473a3174ed
adepamungkas <ade.pamungkas9@gmail.com>
2020-06-24 11:55:35 +0700

    DyestuffChemicalUsageReceiptItemViewModelTest

---
+++ Com.Danliris.Service.Production.Test/ViewModels/DyestuffChemicalUsageReceipt/DyestuffChemicalUsageReceiptItemDetailViewModelTest.cs
@@ -0,0 +1,37 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.DyestuffChemicalUsageReceipt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.ViewModels.DyestuffChemicalUsageReceipt
+{
+    public class DyestuffChemicalUsageReceiptItemDetailViewModelTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            DyestuffChemicalUsageReceiptItemDetailViewModel viewModel = new DyestuffChemicalUsageReceiptItemDetailViewModel()
+            {
+                Index =1,
+                Name = "Name",
+                ReceiptQuantity =1,
+                Adjs1Quantity =1,
+                Adjs2Quantity =1,
+                Adjs3Quantity =1,
+                Adjs4Quantity =1, 
+                Adjs5Quantity=1
+            };
+            Assert.Equal(1, viewModel.Index);
+            Assert.Equal("Name", viewModel.Name);
+            Assert.Equal(1, viewModel.ReceiptQuantity);
+            Assert.Equal(1, viewModel.Adjs1Quantity);
+            Assert.Equal(1, viewModel.Adjs2Quantity);
+            Assert.Equal(1, viewModel.Adjs3Quantity);
+            Assert.Equal(1, viewModel.Adjs4Quantity);
+            Assert.Equal(1, viewModel.Adjs5Quantity = 1
+);
+
+        }
+        }
+}
---
+++ Com.Danliris.Service.Production.Test/ViewModels/DyestuffChemicalUsageReceipt/DyestuffChemicalUsageReceiptItemViewModelTest.cs
@@ -0,0 +1,33 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.DyestuffChemicalUsageReceipt;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.ViewModels.DyestuffChemicalUsageReceipt
+{
+    public class DyestuffChemicalUsageReceiptItemViewModelTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            var time = DateTimeOffset.Now;
+            DyestuffChemicalUsageReceiptItemViewModel viewModel = new DyestuffChemicalUsageReceiptItemViewModel()
+            {
+                ColorCode = "ColorCode",
+                Adjs1Date = time,
+                Adjs2Date =time,
+                Adjs3Date =time,
+                Adjs4Date = time,
+                Adjs5Date = time,
+            };
+
+            Assert.Equal("ColorCode", viewModel.ColorCode);
+            Assert.Equal(time, viewModel.Adjs1Date);
+            Assert.Equal(time, viewModel.Adjs2Date);
+            Assert.Equal(time, viewModel.Adjs3Date);
+            Assert.Equal(time, viewModel.Adjs4Date);
+            Assert.Equal(time, viewModel.Adjs5Date);
+        }
+        }
+}
