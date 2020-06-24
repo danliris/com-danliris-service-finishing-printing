1e3900170fcc3f886e62eff4ff9345ba2d34836b
adepamungkas <ade.pamungkas9@gmail.com>
2020-06-24 17:00:35 +0700

    Create APIEndpointTest.cs

---
+++ Com.Danliris.Service.Production.Test/Helpers/APIEndpointTest.cs
@@ -0,0 +1,28 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.Helpers
+{
+    public class APIEndpointTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            APIEndpoint.Core = "Core";
+            APIEndpoint.Inventory = "Inventory";
+            APIEndpoint.Core = "Core";
+            APIEndpoint.Purchasing = "Purchasing";
+            APIEndpoint.Sales = "Sales";
+            APIEndpoint.Production = "Production";
+
+            Assert.Equal("Core", APIEndpoint.Core);
+            Assert.Equal("Inventory", APIEndpoint.Inventory);
+            Assert.Equal("Production", APIEndpoint.Production);
+            Assert.Equal("Purchasing", APIEndpoint.Purchasing);
+            Assert.Equal("Sales", APIEndpoint.Sales);
+        }
+        }
+}
