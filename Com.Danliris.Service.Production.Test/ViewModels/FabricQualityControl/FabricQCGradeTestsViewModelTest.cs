68c7f02bc7e2ba2f4a1f4df111085e4ddab5abf9
adepamungkas <ade.pamungkas9@gmail.com>
2020-07-13 08:29:48 +0700

    CriteriaViewModelTest

---
+++ Com.Danliris.Service.Production.Test/ViewModels/FabricQualityControl/FabricQCGradeTestsViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.FabricQualityControl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.ViewModels.FabricQualityControl
+{
+    public class FabricQCGradeTestsViewModelTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            FabricQCGradeTestsViewModel viewModel = new FabricQCGradeTestsViewModel()
+            {
+                Grade ="A",
+                OrderNo ="1",
+                OrderQuantity=1
+            };
+
+            Assert.Equal(1,viewModel.OrderQuantity);
+            Assert.Equal("1", viewModel.OrderNo);
+            Assert.Equal("A", viewModel.Grade);
+        }
+            
+    }
+}
---
+++ Com.Danliris.Service.Production.Test/ViewModels/FabricQualityControl/ScoreTest.cs
@@ -0,0 +1,27 @@
+﻿using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.FabricQualityControl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finishing.Printing.Test.ViewModels.FabricQualityControl
+{
+    public class ScoreTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            Score score = new Score()
+            {
+                A=1,
+                B =1,
+                C=1,
+                D =1
+            };
+            Assert.Equal(1, score.A);
+            Assert.Equal(1, score.B);
+            Assert.Equal(1, score.C);
+            Assert.Equal(1, score.D);
+        }
+        }
+}
