bec5fb1f26760059779989bd0111a0aec9323b56
ardana96 <ardanagama@gmail.com>
2021-01-07 14:09:47 +0700

    AddExcelTableFinishingPrinting
    Add table excel Area Pretreatment, Area Preparing, & Area Inventory Greige

---
+++ Com.Danliris.Service.Production.Lib/Models/UploadExcel/Excel_Pretreatment/Excel_AreaPretreatmentChemicalUsedModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Finishing.Printing.WebApi.Controllers.v1.UploadExcel.Excel_Pretreatment
+{
+    public class Excel_AreaPretreatmentChemicalUsedModel
+    {
+        public int Id { get; set; }
+        public string Shift { get; set; }
+        public string Group { get; set; }
+        public string MachineName { get; set; }
+        public string OrderNo { get; set; }
+        public string Material { get; set; }
+        public double? ChemicalPrimalase { get; set; }
+        public double? ChemicalStabironAT2 { get; set; }
+        public double? ChemicalSizolTX { get; set; }
+        public double? ChemicalNaoh { get; set; }
+        public double? ChemicalNeoratePH { get; set; }
+        public double? ChemicalH2o2 { get; set; }
+        public double? ChemicalDsm60 { get; set; }
+        public double? ChemicalDjetpt { get; set; }
+        public double? ChemicalGamasol { get; set; }
+        public string Remark { get; set; }
+
+    }
+}
