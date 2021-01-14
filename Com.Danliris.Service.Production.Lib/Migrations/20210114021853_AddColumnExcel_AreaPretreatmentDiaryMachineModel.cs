a8d9b47527765747b87bc2ac2db6f107d830702c
ardana96 <ardanagama@gmail.com>
2021-01-14 09:36:57 +0700

    ADD ColumnExcel_AreaPretreatmentDiaryMachine

---
+++ Com.Danliris.Service.Production.Lib/Migrations/20210114021853_AddColumnExcel_AreaPretreatmentDiaryMachineModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finishing.Printing.Lib.Migrations
+{
+    public partial class AddColumnExcel_AreaPretreatmentDiaryMachineModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "FlowWaterPolistream",
+                table: "Excel_AreaPretreatmentDiaryMachines",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FlowWaterPolistream2",
+                table: "Excel_AreaPretreatmentDiaryMachines",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FlowWaterWasher",
+                table: "Excel_AreaPretreatmentDiaryMachines",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlowWaterPolistream",
+                table: "Excel_AreaPretreatmentDiaryMachines");
+
+            migrationBuilder.DropColumn(
+                name: "FlowWaterPolistream2",
+                table: "Excel_AreaPretreatmentDiaryMachines");
+
+            migrationBuilder.DropColumn(
+                name: "FlowWaterWasher",
+                table: "Excel_AreaPretreatmentDiaryMachines");
+        }
+    }
+}
