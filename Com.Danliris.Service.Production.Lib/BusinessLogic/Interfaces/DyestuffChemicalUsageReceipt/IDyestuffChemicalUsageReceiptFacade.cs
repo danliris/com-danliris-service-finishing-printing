9ff683758c0413c0b78779d60e17f17031bb12f5
Joifarflex <joifarjosuajosua@gmail.com>
2022-07-07 16:06:00 +0700

    add report DysttuffChemicalUsage

--- Com.Danliris.Service.Production.Lib/BusinessLogic/Interfaces/DyestuffChemicalUsageReceipt/IDyestuffChemicalUsageReceiptFacade.cs
+++ Com.Danliris.Service.Production.Lib/BusinessLogic/Interfaces/DyestuffChemicalUsageReceipt/IDyestuffChemicalUsageReceiptFacade.cs
@@ -1,7 +1,10 @@
 ﻿using Com.Danliris.Service.Finishing.Printing.Lib.Models.DyestuffChemicalUsageReceipt;
+using Com.Danliris.Service.Finishing.Printing.Lib.ViewModels.DyestuffChemicalUsageReceipt;
 using Com.Danliris.Service.Production.Lib.Utilities.BaseInterface;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +13,8 @@ namespace Com.Danliris.Service.Finishing.Printing.Lib.BusinessLogic.Interfaces.D
     public interface IDyestuffChemicalUsageReceiptFacade : IBaseFacade<DyestuffChemicalUsageReceiptModel>
     {
         Task<Tuple<DyestuffChemicalUsageReceiptModel, string>> GetDataByStrikeOff(int strikeOffId);
+        IQueryable<DyestuffChemicalUsageReceiptReportViewModel> GetReportQuery(string productionOrderNo, string strikeOffCode, DateTime? dateFrom, DateTime? dateTo, int offset);
+        Tuple<List<DyestuffChemicalUsageReceiptReportViewModel>, int> GetReport(string productionOrderNo, string strikeOffCode, DateTime? dateFrom, DateTime? dateTo, int page, int size, string Order, int offset);
+        MemoryStream GenerateExcel(string productionOrderNo, string strikeOffCode, DateTime? dateFrom, DateTime? dateTo, int offset);
     }
 }
